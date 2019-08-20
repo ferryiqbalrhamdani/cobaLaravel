33befae0a9786f0702d6032fa5a38b53c7959c3b
ferryiqbalrhamdani <rhamdani128@gmail.com>
2019-08-20 16:39:41 +0700

    meanmbahkan configurasi database

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MahasiswaController extends Controller
 {
@@ -13,7 +15,8 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
-        return view('mahasiswa.index');
+        $mahasiswa = DB::table('mahasiswa')->get();
+        return view('mahasiswa.index', ['mahasiswa' => $mahasiswa]);
     }
 
     /**

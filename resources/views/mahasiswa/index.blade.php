2bd1472d7587604adc540c46707ab7f501c58d0f
ferryiqbalrhamdani <rhamdani128@gmail.com>
2019-08-20 16:48:58 +0700

    menghubungkan data ke database

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -20,16 +20,18 @@
                     </tr>
                 </thead>
                 <tbody>
+                    @foreach($mahasiswa as $mhs)
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Ferry iqbal rhamdani</td>
-                        <td>A11.2016.09634</td>
-                        <td>rhamdani128@gmail.com</td>
-                        <td>Teknik Informatika</td>
+                        <th scope="row">{{ $loop->iteration }}</th>
+                        <td>{{ $mhs->nama }}</td>
+                        <td>{{ $mhs->nim }}</td>
+                        <td>{{ $mhs->email }}</td>
+                        <td>{{ $mhs->jurusan }}</td>
                         <td>
                             <a href="" class="badge badge-success">Ubah</a> | <a href="" class="badge badge-danger">Hapus</a>
                         </td>
                     </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>

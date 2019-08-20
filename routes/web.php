28d7de1669db14d7b825971cc721a3ab48b7605b
ferryiqbalrhamdani <rhamdani128@gmail.com>
2019-08-20 14:38:57 +0700

    menambahkan route pada web.php

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
+
+Route::get('/mahasiswa', 'MahasiswaController@show');
